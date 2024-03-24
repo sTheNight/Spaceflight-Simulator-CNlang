ef0af6aa8ebe5231476ca20f0a9190fd4ec66a1a
sTheNight <sthenight@outlook.com>
2024-03-24 21:05:44 +0800

    update website

--- web/docs/.vitepress/config.mjs
+++ web/docs/.vitepress/config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "航天模拟器汉化包",
+  base: '/Spaceflight-Simulator-CNlang/',
   lang: 'zh-CN',
   description: 'Simplified Chinese language pack for Spaceflight simulator',
   lastUpdated: true,
